9519afb564acef445fbffcac7d13673947ccf5e0
sanjeev_9997 <sanjeevbaghel2002@gmail.com>
2024-07-02 22:56:46 +0530

    push

---
+++ back-end/dbConnection.js
@@ -0,0 +1,12 @@
+const mongoose=require('mongoose')
+
+const connectDB=async()=>{
+    try {
+        await mongoose.connect('mongodb://localhost:27017/bookmyshow');
+        console.log('Connected to Database');
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports=connectDB;
